91e79bd486435901f01c401443fb3c11b04d931f
Noel Espino Córdova <github@ednoesco.com>
2023-09-02 11:36:34 -0600

    2022 - day 1 - calorie counting pt 2

--- 2022/day-1/README.md
+++ 2022/day-1/README.md
@@ -2,5 +2,7 @@
 
 ## Calorie Counting
 
+I'm not proud on the solution but I'm a Rust noob so this needs refactor 🙃
+
 > In Rust I need to add and extra empty space to the input.txt
 > file in order to execute the program successfully
--- 2022/day-1/rust/src/main.rs
+++ 2022/day-1/rust/src/main.rs
@@ -13,24 +13,78 @@ fn main() {
     let input = File::open("../input.txt").expect("File read");
     let entries = BufReader::new(input).lines();
 
-    let mut winning_elf = Elf {
-        number: 0,
-        calories: 0,
-    };
-
     let mut current_elf = Elf {
         number: 1,
         calories: 0,
     };
+
+    let mut winning_elfs = vec![
+        Elf {
+            number: 0,
+            calories: 0,
+        },
+        Elf {
+            number: 0,
+            calories: 0,
+        },
+        Elf {
+            number: 0,
+            calories: 0,
+        },
+    ];
 
     for line in entries {
         match line {
             Ok(line) => match line.parse::<u32>() {
                 Ok(value) => current_elf.calories += value,
                 Err(_) => {
-                    if current_elf.calories > winning_elf.calories {
-                        winning_elf.number = current_elf.number;
-                        winning_elf.calories = current_elf.calories;
+                    println!("Current elf: {:?}", current_elf);
+
+                    if current_elf.calories > winning_elfs[0].calories {
+                        winning_elfs = vec![
+                            Elf {
+                                number: current_elf.number,
+                                calories: current_elf.calories,
+                            },
+                            Elf {
+                                number: winning_elfs[0].number,
+                                calories: winning_elfs[0].calories,
+                            },
+                            Elf {
+                                number: winning_elfs[1].number,
+                                calories: winning_elfs[1].calories,
+                            },
+                        ]
+                    } else if current_elf.calories > winning_elfs[1].calories {
+                        winning_elfs = vec![
+                            Elf {
+                                number: winning_elfs[0].number,
+                                calories: winning_elfs[0].calories,
+                            },
+                            Elf {
+                                number: current_elf.number,
+                                calories: current_elf.calories,
+                            },
+                            Elf {
+                                number: winning_elfs[1].number,
+                                calories: winning_elfs[1].calories,
+                            },
+                        ]
+                    } else if current_elf.calories > winning_elfs[2].calories {
+                        winning_elfs = vec![
+                            Elf {
+                                number: winning_elfs[0].number,
+                                calories: winning_elfs[0].calories,
+                            },
+                            Elf {
+                                number: winning_elfs[1].number,
+                                calories: winning_elfs[1].calories,
+                            },
+                            Elf {
+                                number: current_elf.number,
+                                calories: current_elf.calories,
+                            },
+                        ]
                     }
 
                     current_elf.number += 1;
@@ -41,5 +95,9 @@ fn main() {
         }
     }
 
-    println!("\nWinning elf: {:?}", winning_elf);
+    println!("\nWinning elfs: {:?}", winning_elfs);
+    println!(
+        "Total calories: {:?}",
+        winning_elfs.iter().fold(0, |acc, elf| acc + elf.calories)
+    )
 }
